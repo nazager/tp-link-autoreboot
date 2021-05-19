1770ccab4a10c957a1fdd76c7566c5472fbd26f8
Nazareno Gerbaldo <nazarenogerbaldo@gmail.com>
2021-05-18 21:45:53 -0300

    Working Main

--- src/reboot.py
+++ src/reboot.py
@@ -1,4 +1,12 @@
 from SMWinservice import SMWinservice
+from requests import get
+from base64 import b64encode
+from urllib.request import urlopen
+from urllib.parse import quote
+import time
+import pywintypes
+import win32api
+import sys
 
 tplink = '192.168.0.254'
 user = 'admin'
@@ -20,12 +28,30 @@ class Reboot(SMWinservice):
     def stop(self):
         self.isRunning = True
 
-    
+    @staticmethod
     def checkConnection():
-        pass
+        try:
+            urlopen('https://www.google.com', timeout=10)
+            return True
+        except:
+            return False
 
     def main(self):
-        pass
+        while self.isRunning:
+            if not Rebooter.internet():
+                auth_bytes = bytes(user + ':' + password, 'utf-8')
+                auth_b64_bytes = b64encode(auth_bytes)
+                auth_b64_str = str(auth_b64_bytes, 'utf-8')
+                auth_str = quote('Basic {}'.format(auth_b64_str))
+                auth = {
+                    'Referer': 'http://' + tplink + '/',
+                    'Authorization': auth_str,
+                }
+                reboot_url = url_template.format(tplink)
+                get(reboot_url, headers=auth)
+                time.sleep(120)
+            time.sleep(60)
+
 
 if __name__ == '__main__':
-    pass
+    Rebooter.parse_command_line()
